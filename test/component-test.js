764ba1cf55bc0ce97831f39a2bed37b7fd73f124
Stefan Duberg <stefanduberg@me.com>
2015-09-11 00:03:29 +0200

    add basic test

---
+++ test/component-test.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const Stilar = require('../src/Stilar.js')
+const test = require('tape')
+
+test('component() returns an object with classNames', (t) => {
+  const stilar = Stilar()
+  const styles = stilar.component('Widget', {
+    Widget: {
+      margin: '0',
+      display: 'flex',
+
+      '@media screen and (max-width: 600px)': {
+        padding: '10px',
+      },
+    },
+
+    button: {
+      cursor: 'pointer',
+
+      ':hover': {
+        color: 'red',
+      },
+    },
+  })
+
+  t.equal(styles.Widget, 'Widget')
+  t.equal(styles.button, 'Widget-button')
+  t.end()
+})
+
