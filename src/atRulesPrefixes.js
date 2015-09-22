3c50063489b04db582b192729b4d1bebb5c3009d
Stefan Duberg <stefanduberg@me.com>
2015-09-22 16:03:07 +0200

    update prefixes

---
+++ src/atRulesPrefixes.js
@@ -0,0 +1,7 @@
+'use strict'
+
+export default {
+  '@keyframes': ['webkit'],
+  '@viewport': ['ms'],
+  '@resolution': ['webkit'],
+}
---
+++ src/selectorPrefixes.js
@@ -0,0 +1,8 @@
+'use strict'
+
+export default {
+  '::selection': ['moz'],
+  '::placeholder': ['webkit', 'moz', 'ms'],
+  ':fullscreen': ['webkit', 'moz', 'ms'],
+  '::backdrop': ['webkit', 'ms'],
+}
